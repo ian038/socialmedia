a1394978a4bf2229d40a9a2db8b98746784468a7
ian038 <ianphua3@gmail.com>
2021-01-23 22:28:46 -0500

    Deployed backend

--- spring-mongodb/src/main/java/com/socialmedia/springmongodb/SpringMongodbApplication.java
+++ spring-mongodb/src/main/java/com/socialmedia/springmongodb/SpringMongodbApplication.java
@@ -3,12 +3,10 @@ package com.socialmedia.springmongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringMongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongodbApplication.class, args);
 	}
-
 }
